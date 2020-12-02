d68bfa30935b279912046032e5e8819674902033
Jyles Coad-Ward <jyles@dariox.club>
2020-12-02 17:18:48 +0800

    Changed window dimensions

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ let mainWindow;
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
   });
 
   // and load the index.html of the app.
