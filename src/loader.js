043231788d96ca9de4f7d049b38abdff728ae8e9
Jyles Coad-Ward <jyles@dariox.club>
2020-12-03 03:54:35 +0800

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,13 @@
 module.exports = {
-	
+	show: (content) => {
+		$("#loadingStatus").html(content || "<!-- -->")
+		$("div.preloader").fadeIn("fast")
+	},
+	hide: () => {
+		$("div.preloader").fadeOut("fast")
+		$("#loadingStatus").html("<!-- -->")
+	},
+	caption: (content) => {
+		$("#loadingStatus").html(content || "<!-- -->")
+	}
 }
