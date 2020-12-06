9db53139693d4dbf5f8bc68014abb9f8cdba1c94
Jyles Coad-Ward <jyles@dariox.club>
2020-12-06 22:23:55 +0800

    tag blacklist logic

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,5 @@
+const { localStorage, console, setTimeout } = require("globalthis/implementation");
+
 module.exports = {
 	defaultPage: ()=>{
 		return `
@@ -8,12 +10,12 @@ module.exports = {
 				<h4 id="authSettings_header">Change Login Details</h4>
 				<div class="row">
 					<div class="input-field col s6">
-						<input id="first_name" type="text">
-						<label for="first_name">Username</label>
+						<input id="username" type="text">
+						<label for="username">Username</label>
 					</div>
 					<div class="input-field col s6">
-						<input id="last_name" type="password">
-						<label for="last_name">API Key</label>
+						<input id="key" type="password">
+						<label for="key">API Key</label>
 					</div>
 					<a class="waves-effect waves-light btn" id="authSettings_change">Change & Verify Login Details</a>
 					<a class="waves-effect waves-light btn" id="authSettings_verify">Verify Current Login Details</a>
@@ -23,12 +25,12 @@ module.exports = {
 			<div class="downloadSettings">
 				<h4>Download Settings</h4>
 				<div class="row">
-					Current Download Location <code id="currentDownloadLocation">${localStorage.downloadLocation|| 'No Set Download Location'}</code>
+					Current Download Location <code id="currentDownloadLocation">${localStorage.downloadLocation || 'No Set Download Location'}</code>
 				</div>
 				<div class="row">
 					<div class="changeDownloadLocation">
 						<a class="waves-effect waves-light btn" id="changeDownloadLoc_btn">Change Download Location</a><br>
-						<input placeholder="${localStorage.downloadLocation || 'No Set Download Location'}" id="changeDownloadLocation_textbox" type="text" class="validate">
+						<input placeholder="${localStorage.downloadLocation|| 'No Set Download Location'}" id="changeDownloadLocation_textbox" type="text" class="validate">
 					</div>
 				</div>
 			</div>
@@ -38,6 +40,22 @@ module.exports = {
 				<div class="row">
 					<label for="postsPerPage">Posts per Page</label>
 					<input placeholder="Default: 90; Current: ${localStorage.postsPerPage || '90'}" id="postsPerPage" type="number">
+				</div>
+				<div class="row"> 
+					<div class="currentData tagBlacklist">
+						<label for="chips">Tag Blacklist</label>
+						<div class="chips chips-initial input-field">
+							<!-- every div.chip is generated, everything else in this div is static. there will
+									be some code detecting when enter is pressed (key 13) and it will add
+									the new tag to an array. -->
+							
+							<span class="chipContain">
+
+							</span>
+							<input class="colorScheme input">
+						</div>
+					</div>
+					<i>Note:</i> The e621 enforces the global blacklist (things that would be considered illegal in most countries), and you know what? If you bring up the removal of the global blacklist you're getting a beating.
 				</div>
 				<a class="waves-effect waves-light btn" id="save">save</a>
 			</div>
@@ -65,6 +83,60 @@ module.exports = {
 			return true;
 		}
 	},
+	chip: (storagePopulated)=>{
+		var t_storage = [];
+		if (storagePopulated) {
+			t_storage = localStorage.blacklistedTags.split(",");
+		}
+		module.exports.blacklistedTagsListen();
+	},
+	blacklistedTagsListen: ()=>{
+		var $ = esix.modules.jquery;
+		$("div.apiSettings div.tagBlacklist span.chipContain i.close").click(()=>{
+			setTimeout(()=>{
+				module.exports.updateBlacklistedChipsStorage();
+			},50)
+		})
+		$("div.apiSettings div.tagBlacklist div.chips input").focusin(()=>{
+			$("div.apiSettings div.tagBlacklist div.chips").addClass('input-field-focus');
+		})
+		$("div.apiSettings div.tagBlacklist div.chips input").focusout(()=>{
+			$("div.apiSettings div.tagBlacklist div.chips").removeClass('input-field-focus');
+		})
+		$("div.apiSettings div.tagBlacklist div.chips input").keyup((me)=>{
+			if (me.which == 8 && $("div.apiSettings div.tagBlacklist div.chips input").val().length < 1) {
+				// Remove previous tag
+				var $chipContain = $("div.apiSettings div.tagBlacklist div.chips span.chipContain")
+				console.debug(`[settings.js] Removed blacklistTag '${$chipContain[0].children[$chipContain[0].children.length - 1].children[0].innerHTML}'`)
+				$chipContain[0].children[$chipContain[0].children.length - 1].remove()
+			} else {
+				if (me.which != 13) return;
+				if ($("div.apiSettings div.tagBlacklist div.chips input").val().length < 1) return;
+				$("div.apiSettings div.tagBlacklist span.chipContain").append(`
+				<div class="chip">
+					<span id="tag">${$("div.apiSettings div.tagBlacklist div.chips input").val().trim().split(' ').join("+")}</span>
+					<i class="close material-icons">close</i>
+				</div>
+				`)
+				console.debug(`[settings.js] Added blacklistTag '${$("div.apiSettings div.tagBlacklist div.chips input").val()}'`)
+				$("div.apiSettings div.tagBlacklist div.chips input").val("");
+			}
+			module.exports.updateBlacklistedChipsStorage();
+		})
+	},
+	updateBlacklistedChipsStorage: () => {
+		//$("div.apiSettings div.tagBlacklist div.chips")
+		var chipsInDIV = esix.modules.jquery("div.apiSettings div.tagBlacklist div.chips span.chipContain").children();
+		var finalChips = []
+		Array.from(chipsInDIV).forEach((chip)=>{
+			Array.from(chip.children).forEach((c)=>{
+				if (c.localName.toLowerCase() == 'span') {
+					finalChips.push(c.innerHTML.trim())
+				}
+			})
+		})
+		localStorage.blacklistedTags = finalChips;
+	},
 	listen: ()=>{
 		var $ = require("jquery");
 		var swat = require("sweetalert");
@@ -83,5 +155,25 @@ module.exports = {
 			}
 			module.exports.validate({username:$("div.authSettings input#username[type=text]").val(),key:$("div.authSettings input#key[type=password]").val()})
 		})
+
+		// Blacklisted Tags
+		if (localStorage.blacklistedTags.split(",").length > 1 || localStorage.blacklistedTags != undefined || localStorage.blacklistedTags.length > 1) {
+			var t_blackListedTags = localStorage.blacklistedTags;
+			if (typeof localStorage.blacklistedTags == 'string') {
+				t_blackListedTags = localStorage.blacklistedTags.split(",");
+			}
+			t_blackListedTags.forEach((tag)=>{
+				if (tag.length < 1) return;
+				$("div.apiSettings div.tagBlacklist div.chips span.chipContain").append(`
+					<div class="chip">
+						<span id="tag">${tag}</span>
+						<i class="close material-icons">close</i>
+					</div>
+				`)
+			})
+			module.exports.chip(true);
+		} else {
+			module.exports.chip(false);
+		}
 	}
 }
