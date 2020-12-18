ebffb0034fc407203260ba40d24fac9d161ff23c
Jyles Coad-Ward <jyles@dariox.club>
2020-12-18 20:30:34 +0800

    created keylistener

---
+++ src/keylisten.js
@@ -0,0 +1,43 @@
+var defaultKeymap = {
+	"arrowleft": "previous",
+	"arrowright": "next",
+	"arrowup": "actionup",
+	"arrowdown": "actiondown",
+	"keyd": "download",
+	"escape": "exit",
+	"keyf": "favorite",
+	"keys": "save"
+};
+var listenablePages = [
+	'search',
+	'settings'
+]
+var keymap = defaultKeymap;
+module.exports = () => {
+	console.debug(`[keylisten] => Called`);
+	if (localStorage.keymap == undefined) {
+		localStorage.keymap = JSON.stringify(defaultKeymap)
+		console.debug(`[keylisten] No keymap was found so one was generated.`)
+	} else {
+		keymap = JSON.parse(localStorage.keymap)
+	}
+	console.debug(`[keylisten] Current Keymap`,keymap)
+	document.addEventListener('keydown',module.exports.listener) 
+}
+
+
+
+module.exports.listener = (g_keypress) => {
+	if (!listenablePages.includes(localStorage.currentTab)) return;
+	if (keymap[g_keypress.code.toLowerCase()] == undefined) return;
+	console.debug(`[keylisten] page{${localStorage.currentTab}} => Recieved Valid Keyboard Input '${g_keypress.code}'`)
+	switch(localStorage.currentTab) {
+		case "search":
+			esix.pages.search.keylisten(keymap[g_keypress.code.toLowerCase()])
+			break;
+		case "settings":
+			esix.pages.settings.keylisten(keymap[g_keypress.code.toLowerCase()])
+			break;
+	}
+	return;
+}
