3092847165a3b5b07d335b2f74b1a3e8bb9ab906
jylescoad-ward <jyles@dariox.club>
2020-12-20 01:50:46 +0800

    Changed page content

--- src/home.js
+++ src/home.js
@@ -6,9 +6,9 @@ module.exports = {
 	defaultPage: ()=>{
 		return `
 		<div class="container">
-				<h1>Getting Started</h1>
+				<h1>Welcome to Sixgrid</h1>
 				<p>
-					Information on how to login and use the app would go here.
+					This will probably be replaced with the feed.
 				</p>
 		</div>`;
 	}
