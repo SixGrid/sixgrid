7bfc808f48973190c27d546ff204d267dd7a7270
Jyles Coad-Ward <jylescoadward@gmail.com>
2021-01-08 04:14:24 +0800

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,13 +1,19 @@
 module.exports = {
 	show: (content) => {
+		console.debug("[loader] Shown");
 		$("#loadingStatus").html(content || "<!-- -->")
 		$("div.preloader").fadeIn("fast")
+		$("div.preloader").css("display","inital");
 	},
 	hide: () => {
+		console.debug("[loader] Hidden");
 		$("div.preloader").fadeOut("fast")
 		$("#loadingStatus").html("<!-- -->")
+		$("div.preloader").css("display","none");
 	},
-	caption: (content) => {
+	caption: (content,contentHeader) => {
+		console.debug(`[loader] Caption Changed to '${content}' with header of '${contentHeader || "Loading"}'`);
 		$("#loadingStatus").html(content || "<!-- -->")
+		$("div.preloader div.container h4").html(contentHeader || "Loading");
 	}
 }
