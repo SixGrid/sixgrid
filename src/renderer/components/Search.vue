4838637db791b382af8c241745534598a46b2038
Kate Ward <kate@dariox.club>
2022-01-23 03:34:01 +0800

    Add searchbar

--- src/renderer/components/Search.vue
+++ src/renderer/components/Search.vue
@@ -1,15 +1,27 @@
 <template>
-	<div>
-
+	<div class="container">
+		<div ref="gridview-posts">
+			<md-toolbar class="searchbar md-dense">
+				<md-field>
+					<label>Search Query</label>
+					<md-textarea v-model="searchQuery" md-autogrow></md-textarea>
+				</md-field>
+			</md-toolbar>
+		</div>
 	</div>
 </template>
+<style scoped>
+.container {
+	width: calc(100% - 120px);
+	margin: 25px;
+}
+</style>
 <script>
-import { defineComponent } from '@vue/composition-api'
-
 export default {
 	name: 'Search',
 	data () {
 		return {
+			searchQuery: null,
 			posts: []
 		}
 	}
