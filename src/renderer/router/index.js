ba76068a64b29526eb36264c6c922cfa2981d156
Kate Ward <kate@dariox.club>
2022-01-23 03:34:03 +0800

    Update index.js

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,6 +11,11 @@ export default new Router({
       component: require('@/components/Home').default
     },
     {
+      path: '/search',
+      name: 'search',
+      component: require('@/components/Search').default
+    },
+    {
       path: '*',
       redirect: '/'
     }
