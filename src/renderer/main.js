620f9e72caa48c5beecf68ac3161cba533812b69
Kate Ward <kate@dariox.club>
2022-03-07 18:01:05 +0800

    Fix MdSelect being broken?

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -9,16 +9,27 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 import VueMaterial  from 'vue-material'
+import { MdField } from 'vue-material/dist/components'
 
 Vue.use(VueMaterial)
+Vue.use(MdField)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+
+Vue.component('MdSelect', Vue.options.components.MdSelect.extend({
+    methods: {
+        isInvalidValue: function isInvalidValue () {
+            return this.$el.validity ? this.$el.validity.badInput : this.$el.querySelector('input').validity.badInput
+        }
+    }
+}))
+
 global.vueJS = new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
+    components: { App },
+    router,
+    store,
+    template: '<App/>'
 })
 vueJS.$mount('#app')
