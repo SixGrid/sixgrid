79e5d32521b58da5ce2c34f8c9b227badeec3558
Kate Ward <kate@dariox.club>
2022-03-07 19:06:24 +0800

    Add toastr components

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,14 +5,20 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import VueToastr2 from 'vue-toastr-2'
+import 'vue-toastr-2/dist/vue-toastr-2.min.css'
 
 import VueMaterial  from 'vue-material'
 import { MdField } from 'vue-material/dist/components'
 
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+
+window.toastr = require('toastr')
+
 Vue.use(VueMaterial)
 Vue.use(MdField)
+Vue.use(VueToastr2)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
