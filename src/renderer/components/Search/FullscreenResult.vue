7409aceeca31af69e5fe9be31010bf2e7f390176
Kate Ward <kate@dariox.club>
2022-03-11 20:57:20 +0800

    Ability to go to next and previous post

--- src/renderer/components/Search/FullscreenResult.vue
+++ src/renderer/components/Search/FullscreenResult.vue
@@ -6,15 +6,19 @@
                     <table action="postResult">
                         <tr action="img">
                             <td action="post_prev">
-                                <md-icon>arrow_back</md-icon>
+                                <div action="previous" class="hasclick" @click="prevPost()">
+                                    <md-icon>arrow_back</md-icon>
+                                </div>
                             </td>
-                            <td action="postImage" v-bind:style="`max-height: ${getScreenHeight() - 120}px; max-width: ${getScreenWidth() - 200}px; --img-width: ${postArr[postIndex].Image.File.width > getScreenWidth() - 200 ? getScreenWidth() - 200 : postArr[postIndex].Image.File.width}px; --img-height: ${postArr[postIndex].Image.File.height > getScreenHeight() - 120 ? getScreenHeight() - 120 : postArr[postIndex].Image.File.height}px;`">
-                                <template v-if="postArr[postIndex] != undefined">
+                            <template v-if="postArr[postIndex] != undefined">
+                                <td action="postImage" v-bind:style="`max-height: ${getScreenHeight() - 120}px; max-width: ${getScreenWidth() - 200}px; --img-width: ${postArr[postIndex].Image.File.width > getScreenWidth() - 200 ? getScreenWidth() - 200 : postArr[postIndex].Image.File.width}px; --img-height: ${postArr[postIndex].Image.File.height > getScreenHeight() - 120 ? getScreenHeight() - 120 : postArr[postIndex].Image.File.height}px;`">
                                     <img :src="postArr[postIndex].Image.File.url" />
-                                </template>
-                            </td>
+                                </td>
+                            </template>
                             <td action="post_next">
-                                <md-icon>arrow_forward</md-icon>
+                                <div action="next" class="hasclick" @click="nextPost()">
+                                    <md-icon>arrow_forward</md-icon>
+                                </div>
                             </td>
                         </tr>
                         <tr action="toolbar">
@@ -127,6 +131,19 @@ export default {
         },
         getScreenWidth () {
             return window.innerWidth
+        },
+
+        prevPost () {
+            if (this.$data.postIndex - 1 < 0)
+                return
+
+            this.$set(this.$data, 'postIndex', this.$data.postIndex - 1)
+        },
+        nextPost () {
+            if (this.$data.postIndex + 1 == this.$data.postArr.length)
+                return
+
+            this.$set(this.$data, 'postIndex', this.$data.postIndex + 1)
         }
     }
 }
