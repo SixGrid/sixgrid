5854b4eb909a782eadf5621c655262ef1602a13b
kate <kate@dariox.club>
2022-05-07 23:20:18 +0800

    Create Keybinder.js

---
+++ src/renderer/Keybinder.js
@@ -0,0 +1,11 @@
+// Copyright 2022 Kate Ward <kate@dariox.club> (https://kate.pet).
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+export default class Keybinder {
+    constructor() {
+
+    }
+
+    ConfigBaseDirectory = ''
+    ConfigLocation = ''
+}
