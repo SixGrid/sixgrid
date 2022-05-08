0e8cf3ed6be03e6f8527e3d432d775e12e63f280
Kate Ward <kate@dariox.club>
2022-05-09 01:01:01 +0800

    Update build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+# Copyright 2022 Kate Ward <kate@dariox.club> (https://kate.pet).
+# SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+#!/bin/bash
+echo "======================== NOTICE ========================"
+echo "This build script is meant for kate's build tools. So"
+echo "be warned, some things will break ;w;"
