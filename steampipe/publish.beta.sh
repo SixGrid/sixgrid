38894bd0a76f56234a5bf60dcd5543bc5d84a290
kate <kate@dariox.club>
2022-05-09 13:36:03 +0800

    Create steampipe publish scripts

---
+++ steampipe/publish.beta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ..
+npm i
+npm i --prefix lib
+cd steampipe
+source secret.sh
+cwd__=$(pwd)
+VDF="$cwd__/app_1993420.vdf"
+steamcmd +login $USERNAME $PASSWORD +run_app_build "$VDF" +quit
