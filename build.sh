0a7f836e52603d3bfaf92c0b891a6c2f33d235de
kate <kate@dariox.club>
2022-05-10 13:58:40 +0800

    Restore and pull

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@
 echo "======================== NOTICE ========================"
 echo "This build script is meant for kate's build tools. So"
 echo "be warned, some things will break ;w;"
+
+git restore .
+git pull
+
 cd steampipe
 ./publish.sh
 ./publish.beta.sh
