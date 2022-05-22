2c2d52257fc91fd54e0eed257eba76c47ef72469
kate <kate@dariox.club>
2022-05-22 15:22:30 +0800

    Install modules and build here

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 echo "======================== NOTICE ========================"
 echo "This build script is meant for kate's build tools. So"
-echo "be warned, some things will break ;w;"
+echo "be warned, some things may break when not using that ;w;"
 
 git restore .
 git pull
+
+rm -rf node_modules/
+rm -rf lib/node_modules/
+
+npm i
+npm i --prefix lib
+npm run build:wintux
 
 cd steampipe
 if [ "$git_branch" == "main" ]
