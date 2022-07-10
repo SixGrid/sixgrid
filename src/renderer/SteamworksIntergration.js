78497c59f8e720024f061de31ca60dafd2bcc6fb
kate <kate@dariox.club>
2022-07-10 20:45:29 +0800

    Add counter for uncaughtException

--- src/renderer/SteamworksIntergration.js
+++ src/renderer/SteamworksIntergration.js
@@ -28,9 +28,12 @@ export default class Steamworks extends EventEmitter{
     InitalizeEvents() {
         if (this.hasInitalized) return
         let targets = [
-            'download_completeCount'
+            'download_completeCount',
+            'uncaughtException'
         ]
         for (let i = 0; i < targets.length; i++) {
+            if (this.Metrics[targets[i]].process == undefined)
+                this.Metrics[targets[i]].process = Steamworks.MetricsDefault
             this.on('update', (steamworks) => {
                 this.Metrics[targets[i]] = this.Metrics[targets[i]].process(steamworks, this.Metrics[targets[i]], targets[i])
             })
@@ -40,7 +43,7 @@ export default class Steamworks extends EventEmitter{
         let entries = Object.entries(this.Metrics)
         for (let i = 0; i < entries.length; i++) {
             let pair = entries[i]
-
+            if (pair.incrementOnly) continue
             if (pair.type == 'int') {
                 this.Greenworks.setStatInt(pair.name, pair.default)
             } else if (pair.type == 'float') {
@@ -48,44 +51,54 @@ export default class Steamworks extends EventEmitter{
             }
         } 
     }
+    static MetricsDefault = (steamworks, scope, key) => {
+        if (scope.value > 0) {
+            let currentCount = 0
+            if (scope.type == 'int')
+                currentCount = steamworks.getStatInt(scope.name)
+            else if (scope.type == 'float')
+                currentCount = steamworks.getStatFloat(scope.name)
+            
+            let targetValue = currentCount
+            if (scope.increment) {
+                targetValue = currentCount + scope.value
+                scope.value = 0
+            } else {
+                targetValue = scope.value
+            }
+            let didError = false
+            try {
+                steamworks.setStat(scope.name, targetValue)
+            } catch (err) {
+                console.error(`[SteamworksIntergration->Update] Failed to increment stat ${scope.name}\n`, err)
+                didError = true
+            }
+            if (!didError) {
+                console.debug(`[SteamworksIntergration->Update] Incremented stat (${scope.type}) to ${targetValue} (was ${currentCount})`)
+            }
+        }
+        return scope
+    }
     Metrics = {
         download_completeCount: {
             min: 0,
             max: -1,
             default: 0,
             value: 0,
+            incrementOnly: true,
             name: 'stat_count_downloadpost',
             increment: true,
-            type: 'int',
-            process: (steamworks, scope, key) => {
-                console.log(steamworks)
-                if (scope.value > 0) {
-                    let currentCount = 0
-                    if (scope.type == 'int')
-                        currentCount = steamworks.getStatInt(scope.name)
-                    else if (scope.type == 'float')
-                        currentCount = steamworks.getStatFloat(scope.name)
-                    
-                    let targetValue = currentCount
-                    if (scope.increment) {
-                        targetValue = currentCount + scope.value
-                        scope.value = 0
-                    } else {
-                        targetValue = scope.value
-                    }
-                    let didError = false
-                    try {
-                        steamworks.setStat(scope.name, targetValue)
-                    } catch (err) {
-                        console.error(`[SteamworksIntergration->Update] Failed to increment stat ${scope.name}\n`, err)
-                        didError = true
-                    }
-                    if (!didError) {
-                        console.debug(`[SteamworksIntergration->Update] Incremented stat (${scope.type}) to ${targetValue} (was ${currentCount})`)
-                    }
-                }
-                return scope
-            }
+            type: 'int'
+        },
+        uncaughtException: {
+            min: 0,
+            max: -1,
+            default: 0,
+            value: 0,
+            incrementOnly: true,
+            name: 'stat_count_uncaughtException',
+            increment: true,
+            type: 'int'
         }
     }
     static ERRORS = {
