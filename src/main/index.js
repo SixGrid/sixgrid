b1a00902a35474f84c249c41c72c43343103148f
kate! <kate@dariox.club>
2022-08-28 19:21:52 +0800

    Include new telemetry manager location

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
 import { app, BrowserWindow } from 'electron'
-import TelemetryManager from './telemetryManager'
+import TelemetryManager from './telemetryManager/index'
 import '../renderer/store'
 
 /**
