c2e17a07ac1eeb7b73c787e04b73b947e2f03f09
kate <kate@dariox.club>
2022-09-10 12:09:05 +0800

    Create ConfigTemplate.d.ts

---
+++ src/renderer/ConfigTemplate.d.ts
@@ -0,0 +1,37 @@
+import { IClientAuthOptions } from 'libsixgrid/dist/src/Client'
+import { PostRating } from 'libsixgrid'
+
+export interface AuthTemplate
+{
+    auth: IClientAuthOptions
+    endpoint: string
+}
+export interface IConfig_AuthProfile
+{
+    items: AuthTemplate[]
+    _current: number
+}
+
+export interface IConfig_User
+{
+    media: {
+        autoplay: boolean,
+        loop: boolean
+    },
+    downloadFolder: string
+    saveMetadata: boolean
+    tagBlacklist: string[]
+    ratingFilter: PostRating
+    preloadPageCount: number
+    preloadStartIndex: number
+}
+
+export interface IConfig_Stats
+{
+    download_completeCount: number
+    uncaughtException: number
+    favorite_count: number
+    search_count: number
+    post_downvote_count: number
+    post_upvote_count: number
+}
