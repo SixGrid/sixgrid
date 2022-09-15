b6c5bc268293edf0ad447ad05320b5c03d0c2801
kate! <kate@dariox.club>
2022-09-15 11:28:48 +0800

    Fix console logging and event emitting

--- src/renderer/DownloadManagerBridge.ts
+++ src/renderer/DownloadManagerBridge.ts
@@ -15,21 +15,26 @@ export class DownloadManagerBridge extends EventEmitter
     }
     private async InitializeEvents()
     {
-        ipcRenderer.on('downloadManager:post:download:complete', (event, post: IPostState) =>
+        ipcRenderer.on('downloadManager:post:download:complete', async (event, post: IPostState) =>
         {
-            console.log(post.id)
+            console.log(`[DownloadBridge->downloadManagerPostComplete]`, post.id)
             this.Posts[post.id] = post
+
             this.emit('post:download:complete', post)
+            this.emit('post:state:update', this.Posts[post.id])
         })
-        ipcRenderer.on('downloadManager:post:download:status', (event, post: IPostState) =>
+        ipcRenderer.on('downloadManager:post:download:status', async (event, post: IPostState) =>
         {
-            console.log(post)
-            let diff = post.bytesDownloaded - this.Posts[post.id].bytesDownloaded
+            console.log(`[DownloadBridge->downloadManagerPostStatus]`, post)
+            let previousBytesDownloaded = 0
+            if (this.Posts[post.id] != undefined)
+                previousBytesDownloaded = this.Posts[post.id].bytesDownloaded
+            let diff = post.bytesDownloaded - previousBytesDownloaded
             console.log(diff)
             if (diff > 0)
             {
                 let kbchange = diff / 1e+6
-                console.debug(`[DownloadManagerBridge->postDownloadStatus] ${post.id} ${(post.bytesDownloaded / post.bytesTotal).toFixed(4)}% (${kbchange * 10}mb/s)`)
+                console.debug(`[DownloadManagerBridge->postDownloadStatus] ${post.id} ${((post.bytesDownloaded / post.bytesTotal) * 100).toFixed(4)}% (${kbchange * 10}mb/s)`)
             }
             this.Posts[post.id] = post
             this.emit('post:state:update', this.Posts[post.id])
