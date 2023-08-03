fc7e5eca1ae3fd360a4981cfbfb8a020281b48b0
kate <kate@dariox.club>
2023-08-03 15:11:26 +0800

    Use TelemetryManager

--- src/renderer/notifyProc.ts
+++ src/renderer/notifyProc.ts
@@ -9,33 +9,33 @@ export function notifyProc(data: any) {
             token: AppData.Steamworks.AuthorizationToken,
             data
         }
-        axios.post('http://localhost:5010/api/analytics', pushData).then((d) => {
-            log.debug('data: ', d.data)
-        }).catch((e) => {
-            if (e instanceof AxiosError == false)
-                return;
-            let error = e as AxiosError;
-            let content = `${error.code}`;
-            if (error.response != undefined || error.response != null)
-            {
-                log.error(`${error.response.status}`, error.response.data)
-                content += ` ${error.response.status}`
-                switch (error.response.status)
-                {
-                    case 403:
-                    case 415:
-                        if (error.response.data != undefined)
-                            content += ` ${(error.response.data as any).Message}`
-                        break;
-                    default:
-                        if (error.response.data != undefined)
-                            content += ` ${typeof error.response.data == 'object' ? JSON.stringify(error.response.data) : error.response.data}`
-                        break;
-                }
-            }
-            (global.vueJS as any).$toastr.error(content, 'Telemetry Failed')
-            log.error('Failed to send analytics', e)
-        })
-        // ipcRenderer.send('telemetry:postAction', pushData)
+        // axios.post('http://localhost:5010/api/analytics', pushData).then((d) => {
+        //     log.debug('data: ', d.data)
+        // }).catch((e) => {
+        //     if (e instanceof AxiosError == false)
+        //         return;
+        //     let error = e as AxiosError;
+        //     let content = `${error.code}`;
+        //     if (error.response != undefined || error.response != null)
+        //     {
+        //         log.error(`${error.response.status}`, error.response.data)
+        //         content += ` ${error.response.status}`
+        //         switch (error.response.status)
+        //         {
+        //             case 403:
+        //             case 415:
+        //                 if (error.response.data != undefined)
+        //                     content += ` ${(error.response.data as any).Message}`
+        //                 break;
+        //             default:
+        //                 if (error.response.data != undefined)
+        //                     content += ` ${typeof error.response.data == 'object' ? JSON.stringify(error.response.data) : error.response.data}`
+        //                 break;
+        //         }
+        //     }
+        //     (global.vueJS as any).$toastr.error(content, 'Telemetry Failed')
+        //     log.error('Failed to send analytics', e)
+        // })
+        ipcRenderer.send('telemetry:postAction', pushData)
     }
 }
