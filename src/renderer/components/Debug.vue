9282b6b81fdd66fb610823e691a64580c7ff59ec
kate <kate@dariox.club>
2023-10-13 13:32:48 +0800

    Show internal flags on debug mode

--- src/renderer/components/Debug.vue
+++ src/renderer/components/Debug.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <h3>Routes</h3>
         <ul>
             <template v-for="(route, index) in routes">
                 <template v-if="route.name != undefined">
@@ -7,6 +8,41 @@
                 </template>
             </template>
         </ul>
+        <hr/>
+        <h3>Flags</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Key</th>
+                    <td>State</td>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="(pair, index) in Object.entries(featureFlags)">
+                    <tr v-bind:key="index">
+                        <th>
+                            {{pair[0]}}
+                        </th>
+                        <td>
+                            <template v-if="typeof pair[1] == 'string'">
+                                <code>{{pair[1]}}</code>
+                            </template>
+                            <template v-else-if="typeof pair[1] == 'boolean'">
+                                <teplate v-if="pair[1]">
+                                    ✔️
+                                </teplate>
+                                <template v-else>
+                                    ❌
+                                </template>
+                            </template>
+                            <template v-else>
+                                <code>{{pair[1]}}</code>
+                            </template>
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
     </div>
 </template>
 <script>
@@ -18,7 +54,13 @@ export default {
     methods: {
         initialData () {
             return {
-                routes: vueJS.$router.options.routes
+                routes: vueJS.$router.options.routes,
+                featureFlags: {
+                    allowSteamworks: global.AppData.Helper.AllowSteamworks(),
+                    isDevMode: global.AppData.Helper.IsDevMode(),
+                    isSteamDeck: global.AppData.Helper.IsSteamDeck(),
+                    allowDevEndpointForMetrics: global.AppData.Helper.AllowDevEndpointForMetrics()
+                }
             }
         }
     }
