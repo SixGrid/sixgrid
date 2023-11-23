33041517ba311cd4cceccd7883135009031445f7
kate <kate@dariox.club>
2023-11-23 15:10:20 +0800

    Update helpers.ts

--- src/main/helpers.ts
+++ src/main/helpers.ts
@@ -40,39 +40,6 @@ export function relaunchConfirm () {
         relaunch()
     }
 }
-export function stringArrayCharacterLength (input: string[]) {
-    let length = 0
-    for (let thing of input) {
-        length += thing.length
-    }
-    return length
-}
-export function paragraphSplit (input: string, maximumLineWidth: number) {
-    let resultList = [] /* string[][] */
-    let inputSplitted = input.split(' ')
-    let buffer = [] /* input[] */
-    for (let i = 0; i < inputSplitted.length; i++) {
-        let bufferCharLen = stringArrayCharacterLength(buffer) + buffer.length
-        if (bufferCharLen + inputSplitted[i].length + 1 > maximumLineWidth) {
-            resultList.push(buffer)
-            buffer = []
-        }
-        buffer.push(inputSplitted[i])
-    }
-
-    resultList.push(buffer)
-
-    let resultArray = []
-    for (let i = 0; i < resultList.length; i++) {
-        let tmp = [] /* string[] */
-        for (let x = 0; x < resultList[i].length; x++) {
-            tmp.push(resultList[i][x])
-        }
-        let tmpString = tmp.join(' ')
-        resultArray.push(tmpString)
-    }
-    return resultArray.join('\n')
-}
 
 export function steamCloudConfigDirectory() {
     let target = path.join(path.dirname(process.execPath), 'AppConfig')
