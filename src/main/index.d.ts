7e4158ab5d382361f1afdfbe024fabe6517546c7
kate <kate@dariox.club>
2023-11-23 15:09:50 +0800

    Update index.d.ts

--- src/main/index.d.ts
+++ src/main/index.d.ts
@@ -1,5 +1,6 @@
 import type { IProductInformation } from '../shared'
 import type { BrowserWindow } from 'electron'
+import type { GlobalShortcutData } from '../shared/config'
 declare global {
     namespace NodeJS {
         interface Global {
@@ -8,6 +9,7 @@ declare global {
             __PRODUCT_EXTENDED_INFORMATION: IProductInformation
             __static: string
             debugMode: boolean
+            globalShortcut_data: GlobalShortcutData
         }
     }
 }
