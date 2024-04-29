988256b9308888f9c0d7d4042e52499c53034bf1
kate <kate@dariox.club>
2024-04-29 18:43:23 +0800

    Update build.sh to support flatpak

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ rm -rf node_modules/
 # rm -rf lib/node_modules/
 
 npm i
+npm install --legacy-peer-deps
 # npm i --prefix lib
 
 node ./build-scripts/prebuild.js
@@ -18,6 +19,10 @@ rm -rf build/linux-unpacked
 rm -rf build/win-unpacked
 rm -rf build/artifacts/*.zip
 rm -rf build/artifacts/*.gz
+
+sudo apt install flatpak flatpak-builder -y
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub org.freedesktop.Sdk/x86_64/23.08 org.freedesktop.Platform/x86_64/23.08 app/org.electronjs.Electron2.BaseApp/x86_64/23.08 -y
 
 npm run build:wintux
 
