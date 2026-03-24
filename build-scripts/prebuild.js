8ba8b31c85ca30206726e642e98b319170b74462
kate <kate@dariox.club>
2026-03-24 12:38:17 +0800

    [build-scripts/prebuild] Comment out fix for createHash.js since it seems like it's not required anymore?

--- build-scripts/prebuild.js
+++ build-scripts/prebuild.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-let createHashContent = fs.readFileSync('./node_modules/webpack/lib/util/createHash.js').toString()
-let injectContent = `const crypto = require("crypto");const originalCreateHash = crypto.createHash;crypto.createHash = (...args) => {if (args[0] == 'md4') {return originalCreateHash('md5');}return originalCreateHash(...args);};`
-if (!createHashContent.includes(injectContent)) {
-    createHashContent = `${injectContent}${require('os').EOL}${createHashContent}`
-    fs.writeFileSync('./node_modules/webpack/lib/util/createHash.js', createHashContent)
-}
+// let createHashContent = fs.readFileSync('./node_modules/webpack/lib/util/createHash.js').toString()
+// let injectContent = `const crypto = require("crypto");const originalCreateHash = crypto.createHash;crypto.createHash = (...args) => {if (args[0] == 'md4') {return originalCreateHash('md5');}return originalCreateHash(...args);};`
+// if (!createHashContent.includes(injectContent)) {
+//     createHashContent = `${injectContent}${require('os').EOL}${createHashContent}`
+//     fs.writeFileSync('./node_modules/webpack/lib/util/createHash.js', createHashContent)
+// }
 
 var electronStoreFile = './node_modules/electron-store/index.js';
 var electronStoreReplace = `(electron.app || electron.remote.app).getPath('userData');`
